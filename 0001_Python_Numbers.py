ca2c4275ab64fa9a73128725c5e3fda988bf4f07
MATHI <76192299+mathisudar@users.noreply.github.com>
2022-07-07 20:49:34 +0530

    Create 0001_Python_Numbers.py

---
+++ 0001_Python_Numbers.py
@@ -0,0 +1,98 @@
+
+x1 = 1    # int
+y1 = 2.8  # float
+z1 = 1j   # complex
+
+print("\n")
+print(type(x1),x1)
+print(type(y1),y1)
+print(type(z1),z1)
+print("\n")
+
+
+x2 = 1
+y2 = 35656222554887711
+z2 = -3255522
+
+print("\n")
+print(type(x2),x2)
+print(type(y2),y2)
+print(type(z2),z2)
+print("\n")
+
+
+x3 = 1.10
+y3 = 1.0
+z3 = -35.59
+
+print("\n")
+print(type(x3),x3)
+print(type(y3),y3)
+print(type(z3),z3)
+print("\n")
+
+
+x4 = 35e3
+y4 = 12E4
+z4 = -87.7e100
+
+print("\n")
+print(type(x4),x4)
+print(type(y4),y4)
+print(type(z4),z4)
+print("\n")
+
+
+x5 = 3+5j
+y5 = 5j
+z5 = -5j
+
+print("\n")
+print(type(x5),x5)
+print(type(y5),y5)
+print(type(z5),z5)
+print("\n")
+
+
+
+# Conversion
+
+x6 = 1    # int
+y6 = 2.8  # float
+z6 = 1j   # complex
+
+#convert from int to float:
+a6 = float(x6)
+
+#convert from float to int:
+b6 = int(y6)
+
+#convert from int to complex:
+c6 = complex(x6)
+
+print("\n")
+print(a6)
+print(b6)
+print(c6)
+print("\n")
+
+
+print(type(a6))
+print(type(b6))
+print(type(c6))
+print("\n")
+
+# You cannot convert complex numbers into another number type.
+
+
+
+import random
+
+print(random.randrange(1, 10))
+print(random.randrange(1, 10))
+print(random.randrange(1, 10))
+
+
+
+
+
