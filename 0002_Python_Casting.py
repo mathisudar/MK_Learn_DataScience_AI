f95ac95970605cea576bf00607b6769585acda5e
MATHI <76192299+mathisudar@users.noreply.github.com>
2022-07-08 16:18:23 +0530

    Create 0002_Python_Casting.py

---
+++ 0002_Python_Casting.py
@@ -0,0 +1,40 @@
+
+x1 = int(1)   # x1 will be 1
+y1 = int(2.8) # y1 will be 2
+z1 = int("3") # z1 will be 3
+ 
+print("\n")
+print(x1)
+print(y1)
+print(z1)
+print("\n")
+
+x2 = float(1)     # x2 will be 1.0
+y2 = float(2.8)   # y2 will be 2.8
+z2 = float("3")   # z2 will be 3.0
+w2 = float("4.2") # w2 will be 4.2
+
+print("\n")
+print(x2)
+print(y2)
+print(z2)
+print(w2)
+print("\n")
+
+
+x3 = str("s1") # x3 will be 's1'
+y3 = str(2)    # y3 will be '2'
+z3 = str(3.0)  # z3 will be '3.0'
+
+print("\n")
+print(x3)
+print(y3)
+print(z3)
+print("\n")
+
+
+
+
+
+
+
